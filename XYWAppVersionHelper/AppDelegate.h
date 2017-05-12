1f6fb08bc91de796d02aa028cc204cb4c66a5cb9
xueyognwei <xueyongwei@foxmail.com>
2017-05-12 11:11:15 +0800

    Initial Commit

---
+++ XYWAppVersionHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XYWAppVersionHelper
+//
+//  Created by xueyognwei on 2017/5/12.
+//  Copyright © 2017年 xueyognwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XYWAppVersionHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XYWAppVersionHelper
+//
+//  Created by xueyognwei on 2017/5/12.
+//  Copyright © 2017年 xueyognwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
